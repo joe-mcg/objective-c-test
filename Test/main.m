6facc37b665f58fba9b19f240db06720379000ef
Joe McGuinness <joe.mcguinness@realvnc.com>
2014-10-03 16:13:14 +0100

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Joe McGuinness on 03/10/2014.
+//  Copyright (c) 2014 Joe McGuinness. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
